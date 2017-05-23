030c758d5e8bf70e531be5faa603fc565071e43e
k.takahashi <tk02dr@gmail.com>
2017-05-23 15:07:19 +0900

    tool表示フォーマット

--- source/application.js
+++ source/application.js
@@ -13,3 +13,16 @@ $(function(){
     $("#stone_number").text(parseInt(stone));
   }, 100);
 });
+class Tool {
+  constructor(init_price, name, t_sps){
+    this.price = init_price;
+    this.name  = name;
+    this.t_sps = t_sps;
+    this.count = 0;
+  }
+  buy(){
+    sps += this.t_sps;
+    count++;
+    this.price *= 1.15;
+  }
+}
