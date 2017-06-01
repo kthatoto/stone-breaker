e2a8b9ec5fe2f2eb005273fedf7c65dd489e1256
k.takahashi <tk02dr@gmail.com>
2017-06-01 19:30:26 +0900

    クラスをES5仕様に修正

--- source/application.js
+++ source/application.js
@@ -1,13 +1,13 @@
-var stone = 0;
-var sps   = 0;
+stone = 0;
+sps   = 0;
 $(function(){
-  "use strict";
   kongregateAPI.loadAPI(function(){
     window.kongregate = kongregateAPI.getAPI();
   });
   $("#stone_number").text(stone);
   $("#center_stone").on("click", function(){
     stone++;
+    console.log(stone);
   });
   setInterval(function(){
     stone += sps / 10;
@@ -15,27 +15,36 @@ $(function(){
   }, 100);
   var tool1 = new Tool(10, "hand punch", 0.1, "#tool_1");
 });
-class Tool {
-  constructor(init_price, name, t_sps, tool_id){
-    this.price   = init_price;
+
+
+Tool: {
+  Tool = function(price, name, t_sps, tool_id) {
+    this.price   = price;
     this.name    = name;
     this.t_sps   = t_sps;
     this.count   = 0;
     this.tool_id = tool_id;
-    this.set_info();
-    $(this.tool_id).on("click", this.buy);
-  }
-  set_info(){
+    this.set_info(false);
+    var self = this;
+    $(this.tool_id).on("click", self.buy());
+  };
+  var p = Tool.prototype;
+
+  p.set_info = function(update) {
     var info = $(this.tool_id).find(".tool_info");
-    info.find(".tool_name").text(this.name);
+    if(!update){
+      info.find(".tool_name").text(this.name);
+    }
     info.find(".tool_price_number").text(this.price);
     info.find(".tool_count_number").text(this.count);
     info.find(".tool_sps_number").text(this.t_sps);
-  }
-  buy(){
+  };
+  p.buy = function() {
+    console.log(this.price);
     stone -= this.price;
     sps   += this.t_sps;
     this.count++;
     this.price *= 1.15;
-  }
+    this.set_info(true);
+  };
 }
