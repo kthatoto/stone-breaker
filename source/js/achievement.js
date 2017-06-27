41bd746a7d2d9d9e28e4aac62a86ce921a235ece
tk1to <tk02dr@gmail.com>
2017-06-27 15:34:55 +0900

    achieve整理

--- source/js/achievement.js
+++ source/js/achievement.js
@@ -1,15 +1,23 @@
 
 function achievement_init() {
+  // achievements = {
+  //   "101": new Achievement("tiny punch", ),
+  //   "102": new Achievement("small punch"),
+  //   "103": new Achievement("punch!"),
+  //   "1001": new Achievement("nice click"),
+  //   "1002": new Achievement("nice click"),
+  //   "1003": new Achievement("nice click"),
+  //   "1101": new Achievement("nice click"),
+  //   "1102": new Achievement("nice click"),
+  //   "1103": new Achievement("nice click"),
+  // };
   achievements = {
-    "101": new Achievement("tiny punch"),
-    "102": new Achievement("small punch"),
-    "103": new Achievement("punch!"),
-    "1001": new Achievement("nice click"),
-    "1002": new Achievement("nice click"),
-    "1003": new Achievement("nice click"),
-    "1101": new Achievement("nice click"),
-    "1102": new Achievement("nice click"),
-    "1103": new Achievement("nice click"),
+    "click_count": [
+    ],
+    "click_quantity": [
+    ],
+    "tool0_count": [
+    ],
   };
   $.each(achievements, function(a_id, ach){
     ach.init(a_id);
@@ -17,9 +25,10 @@ function achievement_init() {
 }
 
 Achievement: {
-  Achievement = function(name) {
-    this.name     = name;
-    this.achieved = false;
+  Achievement = function(name, condition) {
+    this.name      = name;
+    this.achieved  = false;
+    this.condition = condition;
   }
   var p = Achievement.prototype;
 
@@ -44,40 +53,43 @@ Achievement: {
   }
 }
 
+function achieve_condition_click(params) {
+}
+
 function achieve_check(category, params) {
-  var au_id = 0;
-  function release_upgrade(u_id){
-    if(upgrades[u_id] && !upgrades[u_id].appear) {
-      upgrades[u_id].init(u_id);
-    }
-  }
-  switch(category){
+  // var au_id = 0;
+  // function release_upgrade(u_id){
+  //   if(upgrades[u_id] && !upgrades[u_id].appear) {
+  //     upgrades[u_id].init(u_id);
+  //   }
+  // }
+  // switch(category){
 
-    case "tool_count":
-      var count = params["count"];
-      var achieve_counts = [10, 25, 50, 100];
-      if(0 <= achieve_counts.indexOf(count)) {
-        au_id = 100*params["t_id"] + achieve_counts.indexOf(count) + 1;
-        achievements[au_id].gain();
-      }
-      break;
+  //   case "tool_count":
+  //     var count = params["count"];
+  //     var achieve_counts = [10, 25, 50, 100];
+  //     if(0 <= achieve_counts.indexOf(count)) {
+  //       au_id = 100*params["t_id"] + achieve_counts.indexOf(count) + 1;
+  //       achievements[au_id].gain();
+  //     }
+  //     break;
 
-    case "click":
-      function click_achieve_check(key_name, achieve_array, base_id){
-        var num = params[key_name];
-        var key = get_count_key(num, achieve_array);
-        if(0 <= key) {
-          au_id = base_id + key + 1;
-          if(achievements[au_id] && !achievements[au_id].achieved){
-            achievements[au_id].gain();
-          }
-          release_upgrade(au_id);
-        }
-      }
-      click_achieve_check("count", [5,10,15], 1000);
-      click_achieve_check("quant", [5,10,15], 1100);
-      break;
-  }
+  //   case "click":
+  //     function click_achieve_check(key_name, achieve_array, base_id){
+  //       var num = params[key_name];
+  //       var key = get_count_key(num, achieve_array);
+  //       if(0 <= key) {
+  //         au_id = base_id + key + 1;
+  //         if(achievements[au_id] && !achievements[au_id].achieved){
+  //           achievements[au_id].gain();
+  //         }
+  //         release_upgrade(au_id);
+  //       }
+  //     }
+  //     click_achieve_check("count", [5,10,15], 1000);
+  //     click_achieve_check("quant", [5,10,15], 1100);
+  //     break;
+  // }
 }
 
 
