7f2fb7d1c1963e503f27b33da6797c0351ee81af
tk1to <tk02dr@gmail.com>
2017-06-30 01:37:40 +0900

    stone切り替え

--- source/js/stone.js
+++ source/js/stone.js
@@ -8,24 +8,40 @@ function stones_init() {
     new Stone(1, 10000, 500, 10000),
   ];
 
+  stones[0].available = true;
   stones[0].set();
 }
 
 
 Stone: {
   Stone = function(stn_id, hp, recover, reward) {
-    this.stn_id  = stn_id;
-    this.max_hp  = hp;
-    this.hp      = hp;
-    this.recover = recover;
-    this.reward  = reward;
-    this.next    = false;
+    this.stn_id    = stn_id;
+    this.max_hp    = hp;
+    this.hp        = hp;
+    this.recover   = recover;
+    this.reward    = reward;
+    this.available = true;
   };
   var p = Stone.prototype;
 
   p.set = function() {
     $("#center_stone").css({
       "background-image": "url(./source/stone_images/stone_"+this.stn_id+".png)"
+    });
+    $("#stone_prev_btn, #stone_next_btn").show().off("click");
+
+    var keys = {"prev": -1, "next": 1};
+
+    var self = this;
+    $.each(keys, function(key, crement){
+      if(stones[self.stn_id + crement] && stones[self.stn_id + crement].available){
+        $("#stone_"+key+"_btn").on("click", function(){
+          stone_id += crement;
+          stones[stone_id].set();
+        });
+      } else {
+        $("#stone_"+key+"_btn").hide();
+      }
     });
   }
 
