ddaaeffc31a0d128c97ab59d7ce6718a7db1bcd3
tk1to <tk02dr@gmail.com>
2017-06-30 00:54:41 +0900

    stone破壊

--- source/js/stone.js
+++ source/js/stone.js
@@ -4,7 +4,8 @@ function stones_init() {
   stone_id = 0;
 
   stones = [
-    new Stone(0, 1000, 50)
+    new Stone(0, 1000,   50,  1000),
+    new Stone(1, 10000, 500, 10000),
   ];
 
   stones[0].set();
@@ -12,11 +13,13 @@ function stones_init() {
 
 
 Stone: {
-  Stone = function(stn_id, hp, recover) {
+  Stone = function(stn_id, hp, recover, reward) {
     this.stn_id  = stn_id;
     this.max_hp  = hp;
     this.hp      = hp;
     this.recover = recover;
+    this.reward  = reward;
+    this.next    = false;
   };
   var p = Stone.prototype;
 
@@ -30,8 +33,15 @@ Stone: {
 
     this.hp -= atk/10;
     this.hp += this.recover/10;
-    if (this.max_hp < this.hp) { this.hp = this.max_hp }
+    if(this.max_hp < this.hp) { this.hp = this.max_hp }
     $("#stone_hp_bar").css("width", float_format(this.hp / this.max_hp, 3)*100 + "%");
+    if(this.hp <= 0){ this.destroy(); }
+  }
+
+  p.destroy = function() {
+    stone += this.reward;
+    // cores[this.stn_id].count++;
+    this.hp = this.max_hp;
   }
 
 
